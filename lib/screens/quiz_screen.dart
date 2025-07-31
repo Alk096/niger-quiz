c2b66934e6cfcd7c0de6ffc58b6c0010fb647710
Alk096 <mahamsdoualker@gmail.com>
2025-07-31 23:25:04 +0100

    Quiz Page reponse

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -8,8 +8,78 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  String? radioValue;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+        title: Text(
+          'Niger Quiz App',
+          style: TextStyle(fontWeight: FontWeight.w400),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(25),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: LinearProgressIndicator(
+                  value: 0.2,
+                  color: Colors.blue,
+                  minHeight: 25,
+                ),
+              ),
+            ),
+            Text(
+              'Quel est la capitale du niger?',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              
+              onTap: (){
+                setState(() {
+                  
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.symmetric(horizontal: 25),
+                height: 45,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Maradi'),
+                    Radio(
+                      focusColor: Colors.blue,
+                      value: 'Maradi',
+                      groupValue: radioValue,
+                      onChanged: (val){
+                        setState(() {
+                          radioValue = val;
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
